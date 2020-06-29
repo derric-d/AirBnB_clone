a5ed39a37a61828dc44ce6bdefb0b9711b473aad
derric-d <derric.donehoo@gmail.com>
2020-06-29 20:29:15 +0000

    add console updates

--- console.py
+++ console.py
@@ -2,6 +2,17 @@
 '''this is the entry point for the interpreter'''
 import cmd
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
+'''
+likely still need:
+from models import storage
+'''
+
 
 class HBNBCommand(cmd.Cmd):
     '''
@@ -10,6 +21,8 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     '''
+    need to do error handling:
+
     ERRORS:
     "** class name missing **",
     "** class doesn't exist **",
@@ -37,7 +50,7 @@ class HBNBCommand(cmd.Cmd):
         # get a valid input from cmd: cls name
         inst = a[0]
 
-        inst.save() # save is base model method
+        inst.save()   # save is base model method
         print(inst.id)
 
     def do_show(self, args):
@@ -47,30 +60,53 @@ class HBNBCommand(cmd.Cmd):
         'show User 123'
         '''
         a = args.split(" ")
+        ret_inst = storage.all()
+        key = "{}.{}".format(a[0], a[1])
+        print(ret_inst[key])
 
     def do_destroy(self, args):
         '''
+        ERROR & storage check
         destroy instances
         'destroy User 123'
         '''
         a = args.split(" ")
+        ret_i = storage.all()
+        key = "{}.{}".format(a[0], a[1])
+        del ret_i[key]
+        storage.save()
 
     def do_all(self, args):
         '''
+        ERROR & storage check
         show all instances , can specify  class
         'all' or 'all User'
         '''
         # retrive all from db
+        all_lst = storage.all()
+        if not arg:
+            print([str(x) for x in all_lst.values()])
+            return
+        a = args.split(" ")
+        print([str(y) for y in all_lst.values()
+              if y.__class__.__name__ == a[0]])
 
     def do_update(self, args):
         '''
+        ERROR & storage check
         update the instance given,
         lookup with name & id and pass attr to update and value to give
         'update User 123 name 'Betty'
         '''
         a = args.split(" ")
         # parse class and attributes and set them
-
+        ret_l = storage.all()
+        key = "{}.{}".format(a[0], a[1])
+        '''
+        for i in range(len(a[1:]) + 1):
+            print(args[i][0])
+        '''
+        # parse and set attr.
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
