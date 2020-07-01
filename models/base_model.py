5f11c3dd6bd20c67c7e17a14a00c89b3fb8e74aa
AndrewC7 <a.cox@me.com>
2020-07-01 16:22:28 +0000

    Add storage functionality

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """str representation of BaseModel"""
@@ -33,6 +34,7 @@ class BaseModel:
         """Update updated_at attribute"""
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return dictionary containing keys/values of __dict__"""
